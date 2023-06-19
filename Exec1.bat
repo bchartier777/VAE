77092a4107bd0bd057de15ea4e5796a797d4cf73
Bernard Chartier <135620107+bchartier777@users.noreply.github.com>
2023-06-19 05:00:33 -0700

    Create Exec1.bat

---
+++ Exec1.bat
@@ -0,0 +1,5 @@
+Rem Batch execution of VAE
+
+python main.py --user_epochs 3  --lr 1e-3 --batch_size 100  --weight_decay 1e-5 --data_folder ./data --seed 3435 1>ExecOut\stdOutVAE.txt  2>ExecOut\stdErrVAE.txt
+
+Rem "Execution complete"
